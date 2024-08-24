061d0bd5e63c4873e3a33411a82dda0e6d815226
Quentin Peris <quentin.peris@icloud.com>
2024-08-24 11:12:08 +0200

    Adresse ok

--- components/AddressInfo.vue
+++ components/AddressInfo.vue
@@ -8,14 +8,14 @@
   <p class="open-sans-font custom-span-contact position-relative">
     <i class="fa fa-envelope-open position-absolute"></i>
     <span class="d-block">mail me</span>
-    <a href="mailto:steve@mail.com">quentin.peris@icloud.com</a>
+    <a href="mailto:quentin.peris@icloud.com">quentin.peris@icloud.com</a>
   </p>
   <!-- {/* End .custom-span-contact */} -->
 
   <p class="open-sans-font custom-span-contact position-relative">
     <i class="fa fa-phone-square position-absolute"></i>
     <span class="d-block">call me</span>
-    <a href="Tel: +216 21 184 010">+33 (0)675124151</a>
+    <a href="Tel: +33675124151">+33 (0)675124151</a>
   </p>
   <!-- {/* End .custom-span-contact */} -->
 </template>
